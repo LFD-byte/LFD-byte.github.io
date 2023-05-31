4ae333667b605be87b52838e5a0e87c8b763069c
admin <huifali@163.com>
2023-05-31 15:24:18 +0800

    Site updated: 2023-05-31 15:24:15

--- js/matery.js
+++ js/matery.js
@@ -77,6 +77,7 @@ $(function () {
             $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
             // 图片添加阴影
             $(this).addClass("img-shadow img-margin");
+            // 为文章内的图片添加 no-referrer 来隐藏 referer （解决第三方图片外链不显示问题）
             // 图片添加字幕
             let alt = $(this).attr('alt');
             let title = $(this).attr('title');
